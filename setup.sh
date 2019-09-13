7e2b9d216734100365f2a9093dc3e3777d1e0b74
Richard Neave <neaverds@icloud.com>
2019-09-13 11:26:43 +0100

    UBUNTU BUILD SCRIPT
    - install golang on ubuntu image
    - install go packages
    - create vagrant test environment

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo add-apt-repository -y ppa:longsleep/golang-backports
+sudo apt-get -y update
+sudo apt-get -y install golang-go
+
 go get "github.com/gin-gonic/gin"
 go get "github.com/satori/go.uuid"
 
