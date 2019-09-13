14693f15c91bc37cf70c7f84033d0d9255596172
Richard Neave <neaverds@icloud.com>
2019-09-13 16:58:36 +0100

    GOLANG INSTALL
    - update golang install script for redhat
    - update gitignore to exclude .vagrant dir

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget https://storage.googleapis.com/golang/go1.11.8.linux-amd64.tar.gz
+curl -O https://storage.googleapis.com/golang/go1.11.8.linux-amd64.tar.gz
 
 tar -xf go1.11.8.linux-amd64.tar.gz && rm go1.11.8.linux-amd64.tar.gz
-mv go /usr/local && mkdir -p /usr/local/gopath
+sudo mv go /usr/local && sudo mkdir -p /usr/local/gopath
 
 echo "set enviornment variables required for Go"
 export GOROOT=/usr/local/go
@@ -11,7 +11,7 @@ export GOPATH=/usr/local/gopath
 cat <<EOF >> ~/.bashrc
 export GOROOT=/usr/local/go
 export GOPATH=/usr/local/gopath
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+export PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH
 EOF
 
 . ~/.bashrc
