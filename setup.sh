9a534b7e9c5a9702e8f80d371bd95b4731990990
Richard Neave <neaverds@icloud.com>
2019-09-13 14:18:30 +0100

    GO INSTALL
    - add installer

--- setup.sh
+++ setup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:longsleep/golang-backports
-sudo apt-get -y update
-sudo apt-get -y install golang-go
+wget https://storage.googleapis.com/golang/go1.11.8.linux-amd64.tar.gz
 
+tar -xf go1.11.8.linux-amd64.tar.gz && rm go1.11.8.linux-amd64.tar.gz
+mv go /usr/local && mkdir -p /usr/local/gopath
+
+echo "set enviornment variables required for Go"
+export GOROOT=/usr/local/go
+export GOPATH=/usr/local/gopath
+cat <<EOF >> ~/.bashrc
+export GOROOT=/usr/local/go
+export GOPATH=/usr/local/gopath
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+EOF
+
+. ~/.bashrc
+
+echo "Installing package - gin gonic"
 go get "github.com/gin-gonic/gin"
+echo "Installing package - go uuid"
 go get "github.com/satori/go.uuid"
 
 
